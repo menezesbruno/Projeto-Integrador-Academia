cdd14961d17410aee1a1ba4f47adefcfb424718f
Bruno Menezes <brunomenezesmg@gmail.com>
2023-04-03 20:58:24 -0300

    Alteração da página inicial e criação de novas páginas: Espaço, Localização e Sobre

---
+++ Academia/Pages/Espaco.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Academia.Pages
+{
+    public class EspacoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
